92fe7d292de66868780c74adaff26c4a02360664
Anuj Rana <Anujranasteam@gmail.com>
2024-01-31 23:35:10 +0530

    form array in angular
    form array in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formArray';
+  form = this.fb.group({
+    lessons: this.fb.array([])
+  });
+  formOptions = [
+    { value: 'one', name: 'one' },
+    { value: 'two', name: 'two' },
+    { value: 'three', name: 'three' }
+  ]
+  currentVal: string[] = [];
+  constructor(private fb: FormBuilder) { }
+
+  get lessons() {
+    return this.form.controls['lessons'] as any;
+  }
+
+  addLesson() {
+    const lessonForm = this.fb.group({
+      level: ['', Validators.required],
+    });
+
+    this.lessons.push(lessonForm);
+  }
+
+  deleteLesson(lessonIndex: number) {
+    this.lessons.removeAt(lessonIndex);
+    this.currentVal.splice(lessonIndex, 1)[0];
+  }
+
+  current({ target }: any) {
+    if (!this.currentVal.includes(target.value)) {
+      this.currentVal.push(target.value);
+    }
+  }
+
 }
